0c56afb8cb08b20d16a0efedafc15b3e952b5ac9
Sebastian García Acosta <segaracos@outlook.com>
2020-11-26 18:24:45 -0500

    Tests initialized

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,7 +1,52 @@
 package ui;
 
-public class Main {
-	public static void main(String[] args) {
-		
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+	
+	Parent root;
+	double xOffset, yOffset;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/fxml/game.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.setFullScreen(true);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Great Bank Manager");
+			primaryStage.show();
+			scene.setFill(Color.TRANSPARENT);
+			root.setOnMousePressed(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					xOffset = event.getSceneX();
+					yOffset = event.getSceneY();
+				}
+			});
+			root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					primaryStage.setX(event.getScreenX() - xOffset);
+					primaryStage.setY(event.getScreenY() - yOffset);
+				}
+			});
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}	
 }
